8bbf4ba39dca9589ab93221ccb2ee0c5144da594
SRVHCCC <chaurasiyahimanshu2002@gmail.com>
2026-01-16 19:15:59 +0530

    done

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,7 @@
-
 const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://api.yourdomain.com"; // <-- replace with your server backend
+    : "https://youngachievers-backend.onrender.com"; // ✅ your backend render url
 
 export default API_BASE_URL;
